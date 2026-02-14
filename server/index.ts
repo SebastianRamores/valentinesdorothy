3b57fa7b4f7296e571de7d73597f7202b8bb342c
Sebastian Ramores <jawyRamores@gmail.com>
2026-02-15 00:56:36 +0800

    add

--- server/index.ts
+++ server/index.ts
@@ -77,9 +77,10 @@ app.use((req, res, next) => {
     // Serve React build in production
     const root = path.join(process.cwd(), "dist"); // or "build" if Vite outputs there
     app.use(express.static(root));
-    app.get("/*", (_req, res) => {
-      res.sendFile(path.join(root, "index.html"));
-    });
+    app.get('/:path(.*)', (_req, res) => {
+  res.sendFile(path.join(root, "index.html"));
+});
+
   } else {
     // Setup Vite dev server in development
     const { setupVite } = await import("./vite");
